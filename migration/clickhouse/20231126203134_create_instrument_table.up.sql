9fef3c9520dd0651b486b934b8f487106dcdf4c9
Lev Nemirovskii <levnemir@yandex.ru>
2025-01-17 13:27:27 +0300

    refactor: delete all structure

--- migration/clickhouse/20231126203134_create_instrument_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS instrument
-(
-    id         Int32,
-    changed    Int32,
-    title      String,
-    short_title String,
-    symbol     String,
-    rate       Float64
-) ENGINE = MergeTree PRIMARY KEY id;
--- migration/clickhouse/20231126211048_create_merchant_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS merchant
-(
-    id      UUID,
-    changed Int32,
-    user    Int32,
-    title   String
-) ENGINE = MergeTree PRIMARY KEY id;
